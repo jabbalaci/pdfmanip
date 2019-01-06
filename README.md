6abb66312137e118d602de366ff6a7391b74941f
Jabba Laci <jabba.laci@gmail.com>
2019-01-06 15:30:28 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# pdfmanip
-Manipulate PDF files (e.g. remove given pages)
+pdfmanip
+========
+
+A simple tool to manipulate your PDF files.
+
+Motivation
+----------
+
+I prefer reading on paper, so I print a lot. Often, when
+I export for instance a blog post in PDF, usually there are
+some pages that I don't want to print.
+
+This little tool helps me removing the unwanted pages easily.
+
+Demo
+----
+
+You must provide the input PDF and then it goes to interactive mode.
+The output is written as `output.pdf`.
+
+```
+$ ./pdfmanip.py input.pdf
+type 'h' for help
+Number of pages: 8
+> h
+h, help        - this help
+q, quit        - quit
+i, info        - info about the status of the output.pdf
+w              - write output.pdf (fails if the file exists)
+w!             - overwrite output.pdf
+d, ls          - directory list
+oi             - open input.pdf with okular
+oo             - open output.pdf with okular
+del PAGES      - delete the given pages
+                 the format of PAGES is like printing pages
+                 example: del 1,2-4,8-
+reload, reset  - reload input.pdf
+                 useful if you deleted some wrong pages and you want to restart
+> del 1,6-
+Number of pages: 4
+# nothing was saved yet
+> w!
+-rw-r--r--   1 jabba users 110024 Jan  6 15:15 output.pdf
+>
+```
+
+Here, the input PDF had 8 pages. After removing pages 1, 6, 7 and 8, we
+got a PDF with 4 remaining pages.
+
+Dependency
+----------
+
+The real PDF manipulation is done with the [pikepdf](https://github.com/pikepdf/pikepdf)
+library.
+
+Supported platform
+------------------
+
+Linux only.
---
+++ pdfmanip.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python3
+
+"""
+pdfmanip
+--------
+
+Manipulate PDF files (e.g. remove given pages)
+
+https://github.com/jabbalaci/pdfmanip
+
+by Laszlo Szathmary (jabba.laci@gmail.com)
+"""
+
+import os
+import readline
+import sys
+from collections import deque
+
+import pikepdf
+
+VERSION = "0.1"
+OUTPUT = 'output.pdf'
+VIEWER = "okular"
+
+
+def usage():
+    text = f"""
+PDF Manipulator v{VERSION} by Laszlo Szathmary (jabba.laci@gmail.com), 2019
+Usage:
+
+pdfmanip input.pdf
+
+Then the program switches to interactive mode.
+""".strip()
+    print(text)
+
+
+def print_help():
+    text = f"""
+h, help        - this help
+q, quit        - quit
+i, info        - info about the status of the output.pdf
+w              - write output.pdf (fails if the file exists)
+w!             - overwrite output.pdf
+d, ls          - directory list
+oi             - open input.pdf with {VIEWER}
+oo             - open output.pdf with {VIEWER}
+del PAGES      - delete the given pages
+                 the format of PAGES is like printing pages
+                 example: del 1,2-4,8-
+reload, reset  - reload input.pdf
+                 useful if you deleted some wrong pages and you want to restart
+""".strip()
+    print(text)
+
+
+def get_pages(s, last):
+    s = s.strip()
+    if len(s) == 0:
+        return []
+
+    li = []
+    #
+    for piece in s.split(','):
+        pos = piece.find('-')
+        if pos > -1:
+            n1 = int(piece[:pos])
+            if piece.endswith('-'):
+                n2 = last
+            else:
+                n2 = int(piece[pos+1:])
+            li.extend(range(n1, n2+1))
+        else:
+            li.append(int(piece))
+    #
+    return li
+
+
+def print_info(pdf):
+    print("Number of pages: {}".format(len(pdf.pages)))
+
+
+def write_file(pdf, overwrite=False):
+    if os.path.isfile(OUTPUT):
+        if not overwrite:
+            print("Warning! The file output.pdf already exists")
+            return
+        #
+    #
+    pdf.save(OUTPUT)
+    os.system(f"ls -al | grep {OUTPUT}")
+
+
+def open_pdf(fname, what):
+    if what == "input":
+        os.system(f"{VIEWER} {fname} &>/dev/null &")
+    if what == "output":
+        if os.path.isfile(OUTPUT):
+            os.system(f"{VIEWER} {OUTPUT} &>/dev/null &")
+        else:
+            print(f"Warning: {OUTPUT} doesn't exist")
+
+
+def delete_pages(pdf, pages):
+
+    def minus_one(q):
+        res = deque([])
+        for n in q:
+            res.append(n - 1)
+        return res
+
+    q = deque(pages)
+    q = minus_one(q)
+    while len(q) > 0:
+        del pdf.pages[q.popleft()]
+        q = minus_one(q)
+    #
+    return pdf
+
+
+def process(fname):
+    if not os.path.isfile(fname):
+        print("Error: the input file doesn't exist", file=sys.stderr)
+        exit(1)
+    # else
+    print("type 'h' for help")
+
+    pdf = pikepdf.open(fname)
+    print_info(pdf)
+
+    while True:
+        try:
+            inp = input("> ").strip()
+        except (KeyboardInterrupt, EOFError):
+            print()
+            print("bye")
+            exit()
+        #
+        if inp in ('h', 'help'):
+            print_help()
+        elif inp in ('q', 'quit'):
+            print("bye")
+            break
+        elif inp in ('i', 'info'):
+            print_info(pdf)
+        elif inp == 'w':
+            write_file(pdf, overwrite=False)
+        elif inp == 'w!':
+            write_file(pdf, overwrite=True)
+        elif inp in ('d', 'ls'):
+            os.system("ls -al")
+        elif inp == 'oi':
+            open_pdf(fname, what="input")
+        elif inp == 'oo':
+            open_pdf(fname, what="output")
+        elif inp == "del":
+            print("provide the pages too")
+        elif inp.startswith("del "):
+            text = "".join(inp.split(" ")[1:])
+            pages = get_pages(text, len(pdf.pages))
+            # print(pages)
+            pdf = delete_pages(pdf, pages)
+            print_info(pdf)
+            print("# nothing was saved yet")
+        elif inp in ("reload", "reset"):
+            pdf = pikepdf.open(fname)
+            print_info(pdf)
+        else:
+            print("What?")
+
+##############################################################################
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        usage()
+        exit(1)
+    # else
+    process(sys.argv[1])
